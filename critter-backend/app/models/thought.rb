7c30629970469ee8fe7fb2efaf8449da2621c517
Richard Li <richardli125@gmail.com>
2019-07-29 18:06:29 -0400

    added migrations

---
+++ critter-backend/app/models/thought.rb
@@ -0,0 +1,9 @@
+class Thought < ApplicationRecord
+  belongs_to :user
+
+  has_many :likes
+  has_many :users, through: :likes
+
+  has_many :replies
+  has_many :users, through: :replies
+end
---
+++ critter-backend/app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  has_many :thoughts
+  has_many :replies
+
+  has_many :likes
+  has_many :thoughts, through: :likes
+end
