5d413538bd0bc583da8d6e46b183d02c31feac20
Richard Li <richardli125@gmail.com>
2019-07-30 15:47:33 -0400

    updated2

--- critter-backend/app/controllers/replies_controller.rb
+++ critter-backend/app/controllers/replies_controller.rb
@@ -1,2 +1,12 @@
 class RepliesController < ApplicationController
+
+  def index
+  end
+
+  def show
+    reply = Reply.find(params[:id])
+    user = reply.user 
+    render json: reply, include: [:user]
+  end
+
 end
